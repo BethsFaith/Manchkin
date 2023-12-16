b5a8063e394b250fb6ced6651e9983f8ba6adb0b
NyanBanan <95487567+NyanBanan@users.noreply.github.com>
2023-12-16 11:42:10 +0300

    Dev (#20)
    * Added:
    -Examples of work
    
    Changes:
    - Stable boosted
    
    * Add content

--- src/Cards/Providers/WearableTreasureCardsProvider.java
+++ src/Cards/Providers/WearableTreasureCardsProvider.java
@@ -74,6 +74,60 @@ public class WearableTreasureCardsProvider implements Deck.Provider {
         WearableTreasureCard leather_armor = new WearableTreasureCard("Кожаный прикид", 200, new ArmorGear(1, false, ArmorGear.Slot.Body));
         cards.add(leather_armor);
 
+        WearableTreasureCard fast_boots = new WearableTreasureCard("Ботинки быстрого бега", 400, new ArmorGear(3, false, ArmorGear.Slot.Legs));
+        fast_boots.setPlay(new WearablePlay() {
+            @Override
+            public boolean WearWithCheck(WearableGear wearable, Person target) {
+                target.changeRunAwayBonus(2);
+                return wearable.wear(target);
+            }
+
+            @Override
+            public boolean UnWearWithCheck(WearableGear wearable, Person target) {
+                target.changeRunAwayBonus(-2);
+                return wearable.unwear(target);
+            }
+        });
+        cards.add(fast_boots);
+
+        WearableTreasureCard morgen_cat = new WearableTreasureCard("Морген-кот", 400, new WeaponGear(3, false, WeaponGear.Size.OneHand));
+        cards.add(morgen_cat);
+
+        WearableTreasureCard knees = new WearableTreasureCard("Шипастые коленки", 200, new ArmorGear(1, false, ArmorGear.Slot.Other));
+        cards.add(knees);
+
+        WearableTreasureCard kitty_mittens = new WearableTreasureCard("Котявые варежки", 300, new ArmorGear(2, false, ArmorGear.Slot.Other));
+        cards.add(kitty_mittens);
+
+        WearableTreasureCard sing_sword = new WearableTreasureCard("Поющий меч", 200, new WeaponGear(2, false, WeaponGear.Size.OneHand));
+        sing_sword.setPlay(checkClass(Person.Class.thief, "вор"));
+        cards.add(sing_sword);
+
         return cards;
     }
+
+    static private WearablePlay checkClass(Person.Class check_class, String class_name) {
+        return new WearablePlay() {
+            @Override
+            public boolean WearWithCheck(WearableGear wearable, Person target) {
+                if (target.getCurClass() != Person.Class.cleric) {
+                    Log.fmtGlobLog("ТЫ ДАЖЕ НЕ %s", class_name);
+                    return false;
+                }
+                wearable.wear(target);
+                return true;
+            }
+
+            @Override
+            public boolean UnWearWithCheck(WearableGear wearable, Person target) {
+                if (target.getCurClass() != Person.Class.cleric) {
+                    Log.fmtGlobLog("ТЫ ДАЖЕ НЕ %s", class_name);
+                    return false;
+                }
+                wearable.unwear(target);
+                return true;
+            }
+        };
+    }
 }
+
